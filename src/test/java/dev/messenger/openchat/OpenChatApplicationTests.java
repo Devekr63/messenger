98071dd0ad92808f3d201a0d29f75a0c89aeb243
Devendra <devekr12@gmail.com>
2024-04-20 11:34:51 +0530

    Project created

---
+++ src/test/java/dev/messenger/openchat/OpenChatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.messenger.openchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
